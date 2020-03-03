3a97ba91733f5d2809289c7df0521983bb8c3917
Shahmir Khan Jadoon <shahmirkj@gmail.com>
2020-03-03 09:08:34 +0500

    New place added feature

--- client/src/components/PlaceItem/PlaceItem.component.js
+++ client/src/components/PlaceItem/PlaceItem.component.js
@@ -1,20 +1,21 @@
 import React from 'react';
+import renderHTML from 'react-render-html';
 import { Media } from 'react-bootstrap';
 
 const PlaceItem = props => {
-	const { name, description, image } = props.place;
+	const { name, description, image_path } = props.place;
 	return (
 		<Media>
 			<img
 				width={100}
 				height={70}
 				className='mr-3'
-				src={`${image}`}
+				src={`http://localhost:5000/${image_path}`}
 				alt='Generic placeholder'
 			/>
 			<Media.Body>
 				<h5>{name}</h5>
-				<p>{description}</p>
+				{renderHTML(description)}
 			</Media.Body>
 		</Media>
 	);
